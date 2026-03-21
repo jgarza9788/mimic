712ac859fc5340ee673f26939bd4a1e301704050
Justin Garza <jgarza9788@gmail.com>
2026-03-21 16:08:13 -0400

    Update install/uninstall scripts to manage user conf.toml

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -4,3 +4,13 @@ set -euo pipefail
 sudo rm -f /usr/local/bin/mimic /usr/local/bin/mimic-msg
 sudo rm -rf /usr/local/share/mimic
 sudo rm -rf /usr/local/share/doc/mimic
+
+if [[ -n "${SUDO_USER:-}" ]]; then
+    target_user="${SUDO_USER}"
+else
+    target_user="$(id -un)"
+fi
+
+target_home="$(eval echo "~${target_user}")"
+rm -f "${target_home}/.config/mimic/conf.toml"
+rmdir --ignore-fail-on-non-empty "${target_home}/.config/mimic" 2>/dev/null || true
